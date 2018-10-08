458f836604d46a449e8b16adb806988c4e50fecd
inkh <jhwe92@gmail.com>
2018-10-08 10:54:03 -0700

    Set up basic methods and userInput prompt

--- PreworkCalculator/Program.cs
+++ PreworkCalculator/Program.cs
@@ -6,12 +6,29 @@ namespace PreworkCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to your simple calculator!");
+            Console.WriteLine("Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division: ");
+            string userInput = Console.ReadLine();
         }
 
         static int Add(int a, int b)
         {
             return a + b;
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double Divide(int a, int b)
+        {
+            return (double)a/b;
         }
     }
 }
